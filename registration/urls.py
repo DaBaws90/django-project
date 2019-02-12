17020b849ca5017abba06f005fee88d74a9e6892
DaBaws90 <43435682+DaBaws90@users.noreply.github.com>
2019-02-12 10:54:19 +0100

    Fixing requirements v2

--- registration/urls.py
+++ registration/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.views.generic import RedirectView
 from .views import SignIn
-from django.contrib.auth.views import login, logout
+# from django.contrib.auth.views import logout
 
 urlpatterns = [
     path('signup/', views.signup, name = "register"),
     # path('signin/', SignIn.as_view(), name = "login"),
     path('signin/', SignIn.as_view(), name = "login"),
     path('profile/', views.profile_update, name = "profile"),
-    path('logout/', logout),
+    path('logout/', include('django.contrib.auth.urls')),
 ]
